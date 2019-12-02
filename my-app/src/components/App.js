10f12ed2ef4b19ce1f86f4b83772e816fe42f959
ilkerakkaya <ilkerakkaya@gmail.com>
2019-12-02 13:34:15 +0100

    first commit

---
+++ my-app/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import './App.css';
+import Navbar from './Navbar'
+import Body from './body'
+import Footer from'./footer'
+
+
+
+
+function App() {
+  return (
+    <div>
+      <Navbar/>
+      <Body/>
+      <Footer/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
